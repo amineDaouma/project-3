1b169b3e555e62c12013db04e1667a620319a658
wanzulfikri <wanzulfikri@gmail.com>
2019-05-25 21:41:53 +0800

    Setup initial Prisma configuration and model

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,11 @@
 const { GraphQLServer } = require("graphql-yoga");
+const { prisma } = require("./generated/prisma-client");
+const Mutation = require("./resolvers/Mutation");
 
-const server = new GraphQLServer({ typeDefs, resolvers });
+const server = new GraphQLServer({
+  typeDefs: "./src/schema.graphql",
+  resolvers: {},
+  context: { prisma }
+});
+
 server.start(() => console.log("Server is running on localhost:4000"));
